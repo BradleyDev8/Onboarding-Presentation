a36ffadd767306bd6aaee295890973e911150151
BradleyDev8 <bradley@hrav.io>
2023-07-24 09:27:42 +0100

    removed bug

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
       <h1 className="text-5xl font-bold">Onboarding with Hamilton Robson</h1>
       <p className="py-6 font-semibold">This presentation will cover the topics that I have learned during the onboarding process at Hamilton Robson</p>
       <button className="btn btn-primary" onClick={() => handleScrollToNext('mac-setup')}>Start Presentation</button>
-      <div className="radial-progress" style={radialStyle}>70%</div>
     </div>
   </div>
 </div>
