62a5974c61d477835d0a585cecb0239431300a87
BradleyDev8 <139467582+BradleyDev8@users.noreply.github.com>
2023-07-24 11:57:24 +0100

    Brad/feat/react page (#2)
    * Added React onboarding page
    
    * react workshop page complete

--- src/containers/UnitTesting.tsx
+++ src/containers/UnitTesting.tsx
@@ -3,9 +3,7 @@ import handleScrollToNext from '../components/scrollToNext';
 
 export default function UnitTesting() {
     
-    const openInNewTab = (url: any) => {
-        window.open(url, '_blank', 'noreferrer');
-    };
+    
 
   return (
     <div id='unit-testing' className="hero min-h-screen bg-base-200 relative">
@@ -17,8 +15,8 @@ export default function UnitTesting() {
       <p className="py-6 font-semibold">The Unit Testing onboarding taught us about the importance of Test Driven Development. Unit testing allows the developer to catch bugs early in the development process, promote code reusability and enable easier refactoring.</p>
       <p className="py-6 font-semibold">The Unit Testing onboarding process included a testing workshop, this had a series of tasks to complete including creating functions and tests for Fetch, Upload, Edit and deleting functions.</p>
       </div>
-      <button onClick={() => handleScrollToNext('git-github')} className="btn btn-secondary mb-4 absolute bottom-0 right-0 mr-4">Next Slide</button>
-      <button role='link' onClick={() => openInNewTab("http://localhost:3001/")} className="btn btn-secondary mb-4 absolute bottom-0 left-0 ml-4">I am A test Button😎</button>
+      <button onClick={() => handleScrollToNext('react-setup')} className="btn btn-secondary mb-4 absolute bottom-0 right-0 mr-4">Next Slide</button>
+      
     </div>
   </div>
 </div>
