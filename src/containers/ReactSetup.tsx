6e0105918da58d06c4f31025918942b247ada49a
BradleyDev8 <bradley@hrav.io>
2023-07-24 11:56:22 +0100

    react workshop page complete

--- src/containers/ReactSetup.tsx
+++ src/containers/ReactSetup.tsx
@@ -1,18 +1,56 @@
-import React from 'react'
-import handleScrollToNext from '../components/scrollToNext';
+import React from "react";
+import handleScrollToNext from "../components/scrollToNext";
+import logo from "../assets/logo.svg";
 
 export default function ReactSetup() {
+  const openInNewTab = (url: any) => {
+    window.open(url, "_blank", "noreferrer");
+  };
   return (
-    <div id='react-setup' className='hero min-h-screen bg-base-100 relative'>
-        <div className='hero-content text-center'>
-            <div className='max-w-md'>
-                <button onClick={()=> handleScrollToNext('unit-testing')} className='btn bg-base-100 border-none text-base-100 hover:bg-accent group absolute top-0 left-0 mt-4 ml-4'>Secret back button<span className='opacity-0 group-hover:opacity-100'>😉</span></button>
-                <div className=' items-center'>
-                    <h1 className='text-5xl  font-bold text-primary'>React</h1>
-                </div>
-                    <p className='py-6 font-semibold'>The React onboarding process included a workshop Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, sint. Doloremque doloribus repellendus, reiciendis nulla assumenda ad optio corrupti adipisci.</p>
-            </div>
+    <div id="react-setup" className="hero min-h-screen bg-base-100 relative ">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <button
+            onClick={() => handleScrollToNext("unit-testing")}
+            className="btn bg-base-100 border-none text-base-100 hover:bg-accent group absolute top-0 left-0 mt-4 ml-4"
+          >
+            Secret back button
+            <span className="opacity-0 group-hover:opacity-100">😉</span>
+          </button>
+          <div className="flex justify-center items-center">
+            <img src={logo} className="App-logo w-32 inline-flex " alt="logo" />
+            <h1 className="text-6xl font-bold text-primary absolute">React</h1>
+            <img
+              src={logo}
+              className="App-logo w-32 inline-flex ml-48"
+              alt="logo"
+            />
+          </div>
+          <p className="py-6 font-semibold">
+            The React onboarding process included a workshop that tasked us with
+            challanges such as:{" "}
+          </p>
+          <div className="flex justify-center font-semibold">
+            <ul className="list-disc mr-6 text-left">
+              <li>Creating a blog Page</li>
+              <li>Add re-usable Nav Bar component</li>
+              <li>Update links to Nav Bar</li>
+              <li>Adding an image</li>
+              <li>Using Flexbox</li>
+              <li>Adding another page</li>
+            </ul>
+          </div>
+          <div className="flex justify-center">
+            <button
+              role="link"
+              onClick={() => openInNewTab("http://localhost:3001/")}
+              className="btn btn-secondary mt-4 absolute"
+            >
+              Click me to see React WorkShop😎
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/containers/UnitTesting.tsx
+++ src/containers/UnitTesting.tsx
@@ -3,9 +3,7 @@ import handleScrollToNext from '../components/scrollToNext';
 
 export default function UnitTesting() {
     
-    const openInNewTab = (url: any) => {
-        window.open(url, '_blank', 'noreferrer');
-    };
+    
 
   return (
     <div id='unit-testing' className="hero min-h-screen bg-base-200 relative">
@@ -18,7 +16,7 @@ export default function UnitTesting() {
       <p className="py-6 font-semibold">The Unit Testing onboarding process included a testing workshop, this had a series of tasks to complete including creating functions and tests for Fetch, Upload, Edit and deleting functions.</p>
       </div>
       <button onClick={() => handleScrollToNext('react-setup')} className="btn btn-secondary mb-4 absolute bottom-0 right-0 mr-4">Next Slide</button>
-      <button role='link' onClick={() => openInNewTab("http://localhost:3001/")} className="btn btn-secondary mb-4 absolute bottom-0 left-0 ml-4">I am A test Button😎</button>
+      
     </div>
   </div>
 </div>
